8f85d3b5fd10c2bb86771798fa7c71395c5534da
Ilie Vlad <ilievlad73@gmail.com>
2021-08-10 16:27:02 +0300

    Improve logs.

--- pkg/plugins/sample/sample.go
+++ pkg/plugins/sample/sample.go
@@ -39,7 +39,7 @@ func (pl *Sample) Name() string {
 }
 
 func (pl *Sample) PreFilter(ctx context.Context, state *framework.CycleState, pod *v1.Pod) *framework.Status {
-	klog.Infof("PREFILTER POD : %v", pod.Name)
+	klog.V(3).Infof("PREFILTER POD : %v", pod.Name)
 	return framework.NewStatus(framework.Success, "")
 }
 
@@ -48,12 +48,12 @@ func (pl *Sample) PreFilterExtensions() framework.PreFilterExtensions {
 }
 
 func (s *Sample) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, node *nodeinfo.NodeInfo) *framework.Status {
-	klog.Infof("FILTER POD : %v", pod.Name)
+	klog.V(3).Infof("FILTER POD : %v", pod.Name)
 	return framework.NewStatus(framework.Success, "")
 }
 
 func (pl *Sample) Score(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) (int64, *framework.Status) {
-	klog.Infof("SCORING POD : %v", pod.Name)
+	klog.V(3).Infof("SCORING POD : %v", pod.Name)
 	return 0, framework.NewStatus(framework.Success, "")
 }
 
@@ -62,12 +62,12 @@ func (pl *Sample) ScoreExtensions() framework.ScoreExtensions {
 }
 
 func (pl *Sample) Reserve(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) *framework.Status {
-	klog.Infof("RESERVE THE POD: %v", pod.Name)
+	klog.V(3).Infof("RESERVE THE POD: %v", pod.Name)
 	return nil
 }
 
 func (pl *Sample) Permit(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) (*framework.Status, time.Duration) {
-	klog.Infof("PERMIT ALLOWS THE POD: %v", pod.Name)
+	klog.V(3).Infof("PERMIT ALLOWS THE POD: %v", pod.Name)
 	return framework.NewStatus(framework.Success, ""), 0
 }
 
@@ -76,12 +76,12 @@ func (pl *Sample) PreBind(ctx context.Context, state *framework.CycleState, pod 
 	// if err != nil {
 	// 	return framework.NewStatus(framework.Error, err.Error())
 	// }
-	klog.Infof("PREBIND NODE : %v", pod.Name)
+	klog.V(3).Infof("PREBIND NODE : %v", pod.Name)
 	return framework.NewStatus(framework.Success, "")
 }
 
 func (pl *Sample) PostBind(ctx context.Context, _ *framework.CycleState, pod *v1.Pod, nodeName string) {
-	klog.Infof("POSTBIND NODE : %v", pod.Name)
+	klog.V(3).Infof("POSTBIND NODE : %v", pod.Name)
 }
 
 func New(plArgs *runtime.Unknown, handle framework.FrameworkHandle) (framework.Plugin, error) {
@@ -89,7 +89,7 @@ func New(plArgs *runtime.Unknown, handle framework.FrameworkHandle) (framework.P
 	if err := framework.DecodeInto(plArgs, args); err != nil {
 		return nil, err
 	}
-	klog.Infof("--------> args: %+v", args)
+	klog.V(3).Infof("--------> args: %+v", args)
 	return &Sample{
 		args:   args,
 		handle: handle,
