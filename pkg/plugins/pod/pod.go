cceb9386a471763b8efcfddf02cde70ecdb8bcea
Ilie Vlad <ilievlad73@gmail.com>
2021-08-13 14:28:16 +0300

    fixup

--- pkg/plugins/pod/pod.go
+++ pkg/plugins/pod/pod.go
@@ -524,9 +524,9 @@ func AreRunninDependsOnPendingMoreThanTwoLayers(pod *v1.Pod, samplePods map[stri
 	appName := AppName(pod)
 	podSample := samplePods[appName]
 	runningDependsOn := podSample.runningDependsOn
-	for _, dependencyPod := range runningDependsOn {
+	for dependencyPodName, dependencyPod := range runningDependsOn {
 		if dependencyPod.status != RUNNING_STATUS && dependencyPod.status != PENDING_STATUS {
-			dependencyRunningDependsOn := samplePods[AppName(dependencyPod)].runningDependsOn
+			dependencyRunningDependsOn := samplePods[dependencyPodName].runningDependsOn
 			for _, secondDependencyPod := range dependencyRunningDependsOn {
 				if secondDependencyPod.status != RUNNING_STATUS && secondDependencyPod.status != PENDING_STATUS {
 					return false
